7fef8c5518dbc100f2dd22e1eaa7843d74666890
mr678 <matthias.reining@outlook.com>
2013-09-04 16:14:28 +0200

    Excel parametrisiert integriert

--- src/test/java/de/herbstcampus/hcxlsbasedtesting/AppTest.java
+++ src/test/java/de/herbstcampus/hcxlsbasedtesting/AppTest.java
@@ -3,6 +3,8 @@ package de.herbstcampus.hcxlsbasedtesting;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 import junit.framework.Assert;
@@ -11,43 +13,54 @@ import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
 /**
  * Unit test for simple App.
  */
+@RunWith(Parameterized.class)
 public class AppTest {
-    
+
+    private List<Object> testRow;
+
+    public AppTest(Object in1, Object in2, Object result) {
+        testRow = Arrays.asList(new Object[]{in1, in2, result});
+    }
+
     @Test
     public void testMultiply() throws IOException {
-        Double actual = new App().multiply(2.0, 2.0);
-        
-        Assert.assertEquals(new Double(4.0), actual);
-        
+        Double in1 = (Double) testRow.get(0);
+        Double in2 = (Double) testRow.get(1);
+        Double expected = (Double) testRow.get(2);
+
+        Double actual = new App().multiply(in1, in2);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Parameterized.Parameters
+    public static Collection dataProvider() throws IOException {
+        List<Object[]> result = new ArrayList<>();
+
         InputStream xlsFile = AppTest.class.getResourceAsStream("/test-cases.xlsx");
         XSSFWorkbook workbook = new XSSFWorkbook(xlsFile);
         XSSFSheet sheet = workbook.getSheetAt(0);
-        
-        Iterator<Row> rowIterator =sheet.iterator();
+
+        Iterator<Row> rowIterator = sheet.iterator();
         while (rowIterator.hasNext()) {
             Row row = rowIterator.next();
-            
+
             List<Object> resultRow = new ArrayList<>();
-            
+
             Iterator<Cell> cellIterator = row.cellIterator();
             while (cellIterator.hasNext()) {
                 Cell cell = cellIterator.next();
                 Double value = new Double(cell.getNumericCellValue());
                 resultRow.add(value);
             }
-            
-            Double in1 = (Double) resultRow.get(0);
-            Double in2 = (Double) resultRow.get(1);
-            Double actual2 = new App().multiply(in1, in2);
-            Double expected = (Double) resultRow.get(2);
-            
-            Assert.assertEquals(expected, actual2);
-            
+            result.add(resultRow.toArray());
         }
+        return result;
     }
-    
 }
